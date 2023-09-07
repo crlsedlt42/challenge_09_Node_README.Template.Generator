72ad4f8d5170195181fd404943b82522554993e4
crlse42 <carlosdlt42@hotmail.com>
2023-09-06 20:55:13 -0700

    Inital Commit of Starter Code and Questions for Generator

---
+++ Develop/index.js
@@ -0,0 +1,105 @@
+// // TODO: Include packages needed for this application
+
+// // TODO: Create an array of questions for user input
+// const questions = [];
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
+
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js");
+
+// -- Turkey and instanbul and the catholic church
+
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
+
+    {
+        type: "input",
+        name: "description",
+        message: "Provide a brief description of your project."
+    },
+
+    {
+        type: "input",
+        name: "usage",
+        message: "Provide instructions for using your project. If you want to include screenshots by creating an assets/images folder in your repository: ![alt text](assets/images/screenshot.png)"
+    },
+
+    {
+        type: "input",
+        name: "contribution",
+        message: "Provide guidelines for contributing to your project."
+    },
+
+    {
+        type: "input",
+        name: "test",
+        message: "Provide instructions for testing your project."
+    },
+
+    {
+        type: "list",
+        name: "license",
+        message: "Select a license for your project.",
+        choices: ["ASU", "Cochise College", "UofA", "GitHub", "None"]
+    },
+
+    {
+        type: "input",
+        name: "github",
+        message: "Enter your GitHub username."
+    },
+
+    {
+        type: "input",
+        name: "githubProfile",
+        message: "Enter the link to your GitHub profile."
+    },
+
+    {
+        type: "input",
+        name: "githubDeployed",
+        message: "Enter the link to your deployed GitHub project."
+    },
+
+];
+
+if (err) {
+    return console.log(err);
+}
+
+console.log("Success! Your README.md file has been generated. Please review the file and make any necessary changes.");
+
+
+// Function call to initialize app
+
+async function init() {
+
+    console.log("Welcome to the README.md generator. Please answer the following questions to generate your README.md file.");
+
+    // Prompt user for answers to following questions
+
+    const answers = await inquirer.prompt(questions);
+    let readmeInfo = generateMarkdown(answers);
+
+    // Creating the README.md file
+
+    writeToFile("(Template)README.md", readmeInfo);
+}
+
+// Function to write README file
+init();
+
+
